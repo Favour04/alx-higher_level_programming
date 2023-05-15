6858fb602da9a1491e2bd84c165694bf6aeb4d49
Favour04 <idaeworfavour1@gmail.com>
2023-05-15 13:58:53 +0100

    done

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-             'u', 'v', 'w', 'x', 'y', 'z']
-    for i in alpha:
-        if c == i:
-            return True
-    return False
+    tempc = ord(c)
+    if tempc > 96 and tempc < 123:
+        return True
+    else:
+        return False
