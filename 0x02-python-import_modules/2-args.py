04ad330df347643d158bdd5612d44cc7c660dba3
Favour04 <idaeworfavour1@gmail.com>
2023-05-16 20:59:26 +0100

    print argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,12 @@
 if __name__ == "__main__":
     from sys import argv
     j = 0
-    print("{} arguments:".format(len(argv) - 1))
+    if len(argv) == 1:
+        print("{} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
     for i in argv:
         if i == argv[0]:
             j += 1
