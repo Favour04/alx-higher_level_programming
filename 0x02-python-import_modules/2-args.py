8c6bf17548fa27f76fcfe3bc973f23a911bab2c8
Favour04 <idaeworfavour1@gmail.com>
2023-05-16 20:51:51 +0100

    print argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    j = 0
+    print("{} argument:".format(len(argv) - 1))
+    for i in argv:
+        if i == argv[0]:
+            j += 1
+            continue
+        else:
+            print("{}: {}".format(j, argv[j]))
+            j += 1
