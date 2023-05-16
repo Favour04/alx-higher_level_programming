b44135c61d52d84fc146b5fec3dafe7c1881aa0c
Favour04 <idaeworfavour1@gmail.com>
2023-05-16 20:53:39 +0100

    print argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     j = 0
-    print("{} argument:".format(len(argv) - 1))
+    print("{} arguments:".format(len(argv) - 1))
     for i in argv:
         if i == argv[0]:
             j += 1
