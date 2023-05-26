3cdad0cf024ac8ec5f3d5fa4184aaa0976f74aca
Favour04 <idaeworfavour1@gmail.com>
2023-05-26 01:32:38 +0100

    done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix is None:
-        return [[[]]]
+        nested_list = [[]]
+        nested_list_str = str(nested_list)
+        padded_list_str = nested_list_str.ljust(10)
+        padded_list = eval(padded_list_str)
+        return padded_list
     temp = []
     for i in matrix:
         k = 0
