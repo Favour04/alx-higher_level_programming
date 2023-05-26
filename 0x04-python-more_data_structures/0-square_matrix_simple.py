bfb765ddb358d5ced1f25c995c676380cf27c6ad
Favour04 <idaeworfavour1@gmail.com>
2023-05-26 01:24:56 +0100

    done

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     if matrix is None:
-        return [[[]]]
+        temp = []
+        temp.append([None])
+        return temp
     temp = []
     for i in matrix:
         k = 0
