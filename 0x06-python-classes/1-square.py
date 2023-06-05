4e96fba8365d50b75d8ff56706b66a2b4244be22
Favour04 <idaeworfavour1@gmail.com>
2023-06-05 19:59:05 +0100

    done

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+ MODULE CONTAINING A CLASS Square
+"""
+
+
+class Square:
+    """
+        CALSS WITH ATRIBUTE OF THE SQUARE SIZE
+    """
+
+    def __init__(self, size):
+        self.__size = size
