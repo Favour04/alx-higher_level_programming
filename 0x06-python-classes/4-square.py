600878d910507c206412401eb30da95e9513aa02
Favour04 <idaeworfavour1@gmail.com>
2023-06-05 21:34:56 +0100

    getter and setter method added to the Square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+    MODULE CONTAINS THE CLASS Square
+"""
+
+
+class Square:
+    """
+        CLASS CONTAIN THE ATTRIBUTE
+        size, WHICH IS THE SIZE OF
+        THE LEGNTH OF THE SQUARE
+     """
+
+    def __init__(self, size=0):
+        """
+            THIS FUNCTION INITIALIZES THE SIZE
+            OF Square
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+
+        if value < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """
+         area FINDS AND RETURN THE AREA OF THE
+         SQUARE. IT USES THE size ATTRIBUTE TO
+         GET THE AREA
+        """
+
+        sqrArea = self.__size * self.__size
+        return sqrArea
