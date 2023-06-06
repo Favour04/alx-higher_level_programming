1f64dac591caa5137efd04bf4c59d2dffef62b0c
Favour04 <idaeworfavour1@gmail.com>
2023-06-06 12:19:55 +0100

    done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,13 +11,14 @@ class Square:
         THE LEGNTH OF THE SQUARE
      """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """
             THIS FUNCTION INITIALIZES THE SIZE
             OF Square
         """
 
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -25,12 +26,36 @@ class Square:
 
     @size.setter
     def size(self, value):
+
         self.__size = value
+
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
 
         if value < 0:
             raise ValueError('size must be >= 0')
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+
+        self.__position = value
+
+        if not isinstance(value, tuple):
+            raise TypeError('position must be a\
+                    tuple of 2 positive integers')
+        else:
+            try:
+                x, y = value
+                if x < 0 or y < 0:
+                    raise TypeError('position must be a\
+                            tuple of 2 positive integers')
+            except Exception:
+                raise TypeError('position must be a\
+                        tuple of 2 positive integers')
 
     def area(self):
         """
@@ -49,7 +74,12 @@ class Square:
 
         if self.size == 0:
             print("")
+
         for i in range(0, self.size):
+            xaxis = self.position[0]
+            if self.position is not None and xaxis != 0:
+                print(" " * xaxis, end='')
             for j in range(0, self.size):
                 print("#", end='')
+
             print("")
