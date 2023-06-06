1fb85ce4e6ae3f2055931d9944c7ca4ce3a4dc69
Favour04 <idaeworfavour1@gmail.com>
2023-06-06 14:39:27 +0100

    coprrected indentation Error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,20 +43,20 @@ class Square:
     def position(self, value):
         if len(value) != 2:
             raise TypeError('position must be a\
-            tuple of 2 positive integers')
+                    tuple of 2 positive integers')
         else:
             try:
                 x, y = value
                 self.__position = value
                 if not isinstance(y, int) or not isinstance(x, int):
                     raise TypeError('position must be a\
-                    tuple of 2 positive integers')
+                            tuple of 2 positive integers')
                 if y < 0 or x < 0:
                     raise TypeError('position must be a\
-                    tuple of 2 positive integers')
+                            tuple of 2 positive integers')
             except Exception:
                 raise TypeError('position must be a\
-                      tuple of 2 positive integers')
+                        tuple of 2 positive integers')
 
     def area(self):
         """
