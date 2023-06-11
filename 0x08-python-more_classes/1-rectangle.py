da0bbf12e487c9bdc4b2d629cc8709baff363cb8
Favour04 <idaeworfavour1@gmail.com>
2023-06-11 21:36:36 +0100

    done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """
-MODLE CONTAINING CLASS RECTANGLE
+    My module is alredy documented
 
 """
 
 
 class Rectangle:
-"""
+
+    """
    Class containing the attributes of a
    Square
-
-"""
+    """
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
@@ -22,7 +22,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise ValueError('width must be an integer')
+            raise TypeError('width must be an integer')
         elif value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
@@ -34,7 +34,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise ValueError('width must be an integer')
+            raise TypeError('height must be an integer')
         elif value < 0:
-            raise ValueError('width must be >= 0')
+            raise ValueError('height must be >= 0')
         self.__height = value
