51decc78c08a466ee069494726484c55082b234c
Favour04 <idaeworfavour1@gmail.com>
2023-06-24 17:19:59 +0100

    lookup module well documented

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+Module containing
+lookup method
+"""
+
 
 def lookup(obj):
+    """
+    This method will be
+    used to list all
+    the method and attri-
+    bute of a class.
+
+    USDAGE
+    ======
+    ::
+
+    lookup(obj)
+    """
+
     return dir(obj)
