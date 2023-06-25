3b04dc5781c8cb7bc2131eff8c5dfed79b5be767
Favour04 <idaeworfavour1@gmail.com>
2023-06-25 23:02:29 +0100

    5-base_geometry.py improved

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module contain
+A class.
+"""
+
+
+class BaseGeometry:
+    """
+    Methods:
+
+    Area
+    ----
+    raises an eception message
+    """
+
+    def area(self):
+        raise Exception('area() is not implemented')
