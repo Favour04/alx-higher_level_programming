eaa049fe3d69b7d3c3d0414406c6a0f0025a473a
Favour04 <idaeworfavour1@gmail.com>
2023-06-25 15:57:40 +0100

    Done

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Moule contain the
+function is_same_class
+which check if an obj
+is from the class a_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    this function as defined
+    in the doc earlier returns
+    True if obj is from the s-
+    ame class as a_class
+    """
+    if type(obj) is a_class:
+        return True
